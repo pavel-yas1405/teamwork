1769aceef4b791006b41f53291a1f16e58d22c0b
pavel-yas1405 <pavel.iasicov99@gmail.com>
2022-03-15 18:23:42 +0200

    table ingredients

--- app/models.py
+++ app/models.py
@@ -11,4 +11,12 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
+class Ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.Text, nullable=True)
+    origin = db.Column(db.String(128), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Ingredients {} {} {}'.format(self.name, self.description, self.origin)
 
