5ab8a54ec5c4eb167a2a6c93c81f6cf09af06600
pavel-yas1405 <pavel.iasicov99@gmail.com>
2022-04-13 22:01:10 +0300

    добавил формы регистрации и авторизации

--- app/models.py
+++ app/models.py
@@ -1,13 +1,26 @@
 from app import db
+from flask_login import UserMixin
 from sqlalchemy import UniqueConstraint
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+    email = db.Column(db.String(50))
     
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -32,10 +45,9 @@ class Cocktail(db.Model):
     description = db.Column(db.Text, nullable=True)
     region = db.Column (db.String(128), index=True)
     recipe = db.Column(db.Text, nullable=True)
-    
+
     def __repr__(self):
         return '<Cocktail {} {} {} {} {}>'.format(self.name, self.description, self.country, self.region, self.recipe)
-
 
 class Ingredient(db.Model):
      __tablename__ = 'ingredient'
@@ -43,7 +55,7 @@ class Ingredient(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(64), index=True, unique=True)
      description = db.Column(db.Text, nullable=True)
-     
+
      def __repr__(self):
          return '<Ingredient {} {}>'.format(self.name, self.description)
 
@@ -52,7 +64,7 @@ class CocktailIngredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cocktail_id = db.Column(db.Integer, db.ForeignKey('cocktail.id', ondelete='CASCADE'), nullable=False)
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id', ondelete='CASCADE'), nullable=False)
-    
+
     __table_args__ = (
     UniqueConstraint(
         'cocktail_id',
